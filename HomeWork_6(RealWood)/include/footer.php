0e4d11edd7de85650b267de41889ffbdedb1b0d9
MrKadachigov <kadachigovs@gmail.com>
2016-11-16 22:14:35 +0200

    home work 6

---
+++ HomeWork_6(RealWood)/include/footer.php
@@ -0,0 +1,49 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <span class="head"><a href="#">Каталог</a></span>
+                <ul>
+                    <li><a href="#">Породы</a></li>
+                    <li><a href="#">Стеновые изделия</a></li>
+                    <li><a href="#">Напольные покрытия</a></li>
+                    <li><a href="#">Лестничные элементы</a></li>
+                    <li><a href="#">Другая продукция</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <span class="head"><a href="#">Покупателям</a></span>
+                <ul>
+                    <li><a href="#">Схема проезда</a></li>
+                    <li><a href="#">Доставка</a></li>
+                    <li><a href="#">Кредит</a></li>
+                    <li><a href="#">Полезно знать</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <span class="head"><a href="#">Realwood</a></span>
+                <ul>
+                    <li><a href="#">Филиалы</a></li>
+                    <li><a href="#">О компании</a></li>
+                    <li><a href="#">Производства</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3 logo-box">
+                <span class="head"><a href="#"><img src="img/footer-logo.png" alt=""></a></span>
+                <ul>
+                    <li><span><a href="#">(123) 456-78-99</a></span><span><a href="#">(123) 456-78-99</a></span></li>
+                    <li><span><a href="#">(123) 456-78-99</a></span><span><a href="#">(123) 456-78-99</a></span></li>
+                    <li><a href="#">s-leswood@ya.ru</a></li>
+                    <li><a href="#">МО, ПО. Путилково 69км. МКАД,<br>ЗАО Гринвуд, строение 9, оф. 85<br>(от МКАД 200 метров)</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ HomeWork_6(RealWood)/include/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="header">
+    <div class="header-top">
+        <div class="container">
+            <div class="header-logo">
+                <img src="img/logo.png" alt="">
+            </div>
+            <div class="header-text pull-left">
+                <p>Производство и продажа отделочных
+                    материалов из натурального дерева</p>
+            </div>
+            <div class="header-contact pull-left">
+                <ul class="list-inline">
+                    <li><a href="#">(926) 779-79-74</a></li>
+                    <li><a href="#">(495) 940-76-99</a></li>
+                    <li>|</li>
+                    <li><a href="#">s-leswood@ya.ru</a></li>
+                </ul>
+            </div>
+            <div class="header-icons pull-right">
+                <ul class="list-inline">
+                    <li><a class="pad" href="#"></a></li>
+                    <li><a class="tel" href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="header-nav">
+        <div class="container">
+            <ul class="list-inline pull-left">
+                <li class="active"><a href="#">Продукция</a></li>
+                <li><a href="#">Цены</a></li>
+                <li><a href="#">Фотогалерея</a></li>
+                <li><a href="#">Статьи</a></li>
+                <li><a href="#">Где купить</a></li>
+                <li><a href="#">Контакты</a></li>
+            </ul>
+            <div class="search-form pull-right">
+                <form>
+                    <div class="input-group">
+                        <label>
+                            <input type="text" class="form-control">
+                        </label>
+                        <span class="input-group-btn">
+                                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+                            </span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
